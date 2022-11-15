651d3a785edb91ad0270f201246b05967e87f36e
ngjinshan <ngjinshan99@gmail.com>
2022-11-16 00:09:27 +1100

    updated barchart

--- src/components/barchart/proficiency.js
+++ src/components/barchart/proficiency.js
@@ -1,62 +1,54 @@
 export const proficiency = [
     {
-        "name": "React",
-        "value": 90
+        name: "React",
+        value: 90,
     },
     {
-        "name": "Javascript",
-        "value": 80
+        name: "Javascript",
+        value: 80,
     },
     {
-        "name": "HTML5",
-        "value": 80
+        name: "HTML5",
+        value: 80,
     },
     {
-        "name": "CSS",
-        "value": 80
+        name: "CSS",
+        value: 80,
     },
     {
-        "name": "Typescript",
-        "value": 70
+        name: "Typescript",
+        value: 70,
     },
     {
-        "name": "MongoDB",
-        "value": 70
+        name: "MongoDB",
+        value: 70,
     },
     {
-        "name": "Express",
-        "value": 70
+        name: "Express",
+        value: 70,
     },
     {
-        "name": "Git",
-        "value": 70
+        name: "Git",
+        value: 60,
     },
     {
-        "name": "NestJS",
-        "value": 60
+        name: "Python",
+        value: 60,
     },
     {
-        "name": "NextJS",
-        "value": 60
+        name: "SQL",
+        value: 60,
     },
     {
-        "name": "Python",
-        "value": 60
+        name: "Django",
+        value: 60,
     },
     {
-        "name": "SQL",
-        "value": 60
+        name: "Java",
+        value: 50,
     },
     {
-        "name": "Django",
-        "value": 60
+        name: "PHP",
+        value: 50,
     },
-    {
-        "name": "Java",
-        "value": 50
-    },
-    {
-        "name": "PHP",
-        "value": 50
-    }
-]
+];
