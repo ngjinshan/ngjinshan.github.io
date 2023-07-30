08995090f9a52e21a49137bd1366154338e7fe57
Jin Shan Ng <ngjinshan99@gmail.com>
2023-07-30 14:48:07 +1000

    update

--- src/components/barchart/proficiency.js
+++ src/components/barchart/proficiency.js
@@ -1,54 +1,66 @@
 export const proficiency = [
-    {
-        name: "React",
-        value: 90,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "HTML5",
-        value: 80,
-    },
-    {
-        name: "CSS",
-        value: 80,
-    },
-    {
-        name: "Typescript",
-        value: 70,
-    },
-    {
-        name: "MongoDB",
-        value: 70,
-    },
-    {
-        name: "Express",
-        value: 70,
-    },
-    {
-        name: "Git",
-        value: 60,
-    },
-    {
-        name: "Python",
-        value: 60,
-    },
-    {
-        name: "SQL",
-        value: 60,
-    },
-    {
-        name: "Django",
-        value: 60,
-    },
-    {
-        name: "Java",
-        value: 50,
-    },
-    {
-        name: "PHP",
-        value: 50,
-    },
+  {
+    name: "React",
+    value: 90,
+  },
+  {
+    name: "Typescript",
+    value: 90,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "HTML5",
+    value: 80,
+  },
+  {
+    name: "CSS",
+    value: 80,
+  },
+  {
+    name: "MongoDB",
+    value: 70,
+  },
+  {
+    name: "SQL",
+    value: 70,
+  },
+  {
+    name: "Express",
+    value: 70,
+  },
+  {
+    name: "Git",
+    value: 70,
+  },
+  {
+    name: "Python",
+    value: 60,
+  },
+  {
+    name: "Django",
+    value: 60,
+  },
+  {
+    name: "Docker",
+    value: 60,
+  },
+  {
+    name: "NGINX",
+    value: 60,
+  },
+  {
+    name: "AWS",
+    value: 50,
+  },
+  {
+    name: "Java",
+    value: 50,
+  },
+  {
+    name: "PHP",
+    value: 50,
+  },
 ];
--- src/components/notes/experience.js
+++ src/components/notes/experience.js
@@ -1,54 +1,47 @@
 export const experience = [
-    {
-        title: "Software Developer",
-        company: "Monash University",
-        date: "Oct '22 - Present",
-        detail: ["Designing relational database with more than 15 tables and implementing on Airtable to migrate from manual record keeping process on Google Drive",
-        "Designing and developing dashboards and interfaces on Airtable to keep track of companies that have or had contracts with Monash",
-        "Writing custom Airtable scripts in JavaScript to manipulate data and automate workflow",
-        "In charge of discussing and collaborating with different teams and departments in Monash to meet business requirements"],
-    },
-    {
-        title: "Information Technology Project Officer",
-        company: "Monash University",
-        date: "Apr '23 - May '23",
-        detail: ["Developed the frontend interface using React Typescript for a research project related to synthetic speech generation AI",
-    "Developed a web app using React Typescript and Flask for a research project related to object detection AI"],
-    },
-    {
-        title: "Research Assistant",
-        company: "Monash University",
-        date: "May '22 - Jul '22",
-        detail: ["Developed the frontend interface for a project related to NLP using React"],
-    },
-    {
-        title: "Business Analyst Intern",
-        company: "Fusionex",
-        date: "Nov '21 - Jan '22",
-        detail: ["Involved in full-stack development of a core product, GIANT, using ASP.NET Core, Vue.js and MongoDB"],
-    },
-    {
-        title: "Web Dev Intern",
-        company: "Dassault Systemes",
-        date: "Jun '21 - Nov '21",
-        detail: ["Involved in full-stack development of in-house service portal using PHP, JavaScript, HTML5, CSS and MySQL"],
-    },
-    {
-        title: "Research Assistant",
-        company: "Monash Malaysia",
-        date: "Apr '21 - Oct '21",
-        detail: [
-            "Trained a deep-learning AI model to diagnose breast cancer using histopathological images using PyTorch along with Lime to explain the model results",
-            "Developed API to use that trained model using Flask and a website using React",
-        ],
-    },
-    {
-        title: "Software Engineer Intern",
-        company: "Curlec",
-        date: "Dec '20 - Feb '21",
-        detail: [
-            "Spearheaded a demo bank system which demonstrates a new real-time direct debit transaction using the MERN stack",
-            "Developed a new subscription-based system to offer their services/products as subscriptions using React, DjangoRestFramework and PostgreSQL",
-        ],
-    },
+  {
+    title: "Software Developer",
+    company: "Monash University",
+    date: "Oct '22 - Present",
+    detail: [
+      "Designing relational database with more than 15 tables and implementing on Airtable to migrate from manual record keeping process on Google Drive",
+      "Developing dashboards and interfaces on Airtable to monitor companies with agreements with Monash, while implementing custom JavaScript scripts for automating data manipulation and workflow tasks",
+      "Designing and developing the Companies on Campus client portal using React.ts, Express.js, Docker andNGINX, AWS EC2",
+      "In charge of discussing and collaborating with different teams and departments in Monash to meet business requirements",
+    ],
+  },
+  {
+    title: "Business Analyst Intern",
+    company: "Fusionex",
+    date: "Nov '21 - Jan '22",
+    detail: [
+      "Involved in full-stack development of a core product, GIANT, using ASP.NET Core, Vue.js and MongoDB",
+    ],
+  },
+  {
+    title: "Web Dev Intern",
+    company: "Dassault Systemes",
+    date: "Jun '21 - Nov '21",
+    detail: [
+      "Involved in full-stack development of in-house service portal using PHP, JavaScript, HTML5, CSS and MySQL",
+    ],
+  },
+  {
+    title: "Research Assistant",
+    company: "Monash Malaysia",
+    date: "Apr '21 - Oct '21",
+    detail: [
+      "Trained a deep-learning AI model to diagnose breast cancer using histopathological images using PyTorch along with Lime to explain the model results",
+      "Developed API to use that trained model using Flask and a website using React",
+    ],
+  },
+  {
+    title: "Software Engineer Intern",
+    company: "Curlec",
+    date: "Dec '20 - Feb '21",
+    detail: [
+      "Spearheaded a demo bank system which demonstrates a new real-time direct debit transaction using the MERN stack and AWS EC2",
+      "Developed a new subscription-based system to offer their services/products as subscriptions using React, DjangoRestFramework and PostgreSQL",
+    ],
+  },
 ];
